c0659c14f0d1d7eace5a5bf689f8e599055108fd
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 19:47:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index {|name, index| puts "The line is currently: #{index + 1}. #{name}"}
+    line_prompt = "The line is currently: "
+    katz_deli.each_with_index do |name, index|
+
+    end 
   end
 end
